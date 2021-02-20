852771d7592886aadb4fc30bc0ea1922793ab521
MercybirungiS <mercyssbirungi@gmail.com>
2021-02-21 00:27:06 +0300

    Mobile development assignment

---
+++ src/mobile development assignment.kt
@@ -0,0 +1,14 @@
+fun main() {
+    var nameOfStudent="Hesed Kisakye "
+    var ageOfStudent=15
+    var weightOfStudent="45kg"
+    var phoneNumber="0780256885"
+    var citizenship= "Kenyan"
+    var trueORfalse= false
+    println(nameOfStudent)
+    println(ageOfStudent)
+    println(weightOfStudent)
+    println(phoneNumber)
+    println(citizenship)
+    println(trueORfalse)
+}
